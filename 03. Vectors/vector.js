aaba6612ec72c4e0036de44af52e08e8eb02b3be
Derrick Sebesta <dsebesta@gmail.com>
2017-02-22 23:17:36 -0800

    finished vectors

--- 03. Vectors/vector.js
+++ 03. Vectors/vector.js
@@ -15,31 +15,50 @@
 // [ ] .contains() function to return true/false whether a given value exists in the Vector
 //
 var Vector = function(initialCapacity, maxCapacity) {
-  this.storage = ...;
-  this.capacity = initialcapacity || 16;  // Default to array size 16
-  this.max = maxCapacity || 1 << 24;      // Default to max Vector size 16,777,216
-  this.length = 0;
+    this.storage = [];
+    this.capacity = initialCapacity || 16;  // Default to array size 16
+    this.max = maxCapacity || 1 << 24;      // Default to max Vector size 16,777,216
+    this.length = 0;
 };
 
 
 Vector.prototype.insert = function(index, value) {
-  // ...
+    for (var i = this.length; i > index; i--) {
+        this.set(i, this.storage[i-1])
+    }
+    this.set(index, value);
+    this.length++
+
 };
 
 Vector.prototype.add = function(value) {
-  // ...
+    this.set(this.length, value);
+    this.length++
 };
 
 Vector.prototype.remove = function(index) {
-  // ...
+    for (var i = index; i < this.length - 1; i ++) {
+        this.set(i, this.storage[i+1])
+    }
+    this.set(this.length - 1, null);
+    this.length--
 };
 
 Vector.prototype.get = function(index) {
-  // ...
+    return this.storage[index];
 };
 
 Vector.prototype.set = function(index, value) {
-  // ...
+    this.storage[index] = value;
+};
+
+Vector.prototype.contains = function (value) {
+    for (var i = 0; i < this.length; i++) {
+        if (value === this.get(i)) {
+            return true
+        }
+    }
+    return false
 };
 
 
@@ -58,3 +77,6 @@ v.set(2, 15);
 console.log("v.set(2, 15): " + (v.get(2) === 15));
 v.remove(2);
 console.log("v.remove(2): " + (v.length === 5));
+console.log("contains(3): " + (v.contains(3)));
+
+
