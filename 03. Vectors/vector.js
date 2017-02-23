5e5b180607b2f3d9f394a137b5d4fee5b037cce0
Derrick Sebesta <dsebesta@gmail.com>
2017-02-23 00:23:16 -0800

    bounds and capacity added. still need to work on max capacity and capacity removal upon delete

--- 03. Vectors/vector.js
+++ 03. Vectors/vector.js
@@ -6,34 +6,53 @@
 // NOTE: Must implement from scratch, cannot create a direct wrapper around built-in functionality
 //
 // [ ] implement a Vector class with the following functions
-// [ ] .insert() function that takes an index and value to insert
-// [ ] .add() function to append a value to the end of the array
-// [ ] .remove() function to remove whatever value at index
-// [ ] .get() function to return value at index
-// [ ] .set() function to set value at index
+// [X] .insert() function that takes an index and value to insert
+// [X] .add() function to append a value to the end of the array
+// [X] .remove() function to remove whatever value at index
+// [X] .get() function to return value at index
+// [X] .set() function to set value at index
 //
-// [ ] .contains() function to return true/false whether a given value exists in the Vector
+// [X] .contains() function to return true/false whether a given value exists in the Vector
 //
 var Vector = function(initialCapacity, maxCapacity) {
     this.storage = [];
     this.capacity = initialCapacity || 16;  // Default to array size 16
     this.max = maxCapacity || 1 << 24;      // Default to max Vector size 16,777,216
     this.length = 0;
+    this.setStorage();
 };
 
+Vector.prototype.setStorage = function() {
+    for (var i = this.length; i < this.capacity; i++) {
+        this.storage[i] = null;
+    }
+};
+
+Vector.prototype.checkCapacity = function (upcomingIndex) {
+    if (upcomingIndex > this.capacity) {
+        this.increaseCapacity();
+    }
+};
+
+Vector.prototype.increaseCapacity = function() {
+    this.capacity *= 2;
+    this.setStorage();
+};
 
 Vector.prototype.insert = function(index, value) {
+    if (this.boundsCheck(index)) {return}
+    this.checkCapacity(this.length+1);
     for (var i = this.length; i > index; i--) {
         this.set(i, this.storage[i-1])
     }
     this.set(index, value);
-    this.length++
-
+    this.length++;
 };
 
 Vector.prototype.add = function(value) {
+    this.checkCapacity(this.length+1);
     this.set(this.length, value);
-    this.length++
+    this.length++;
 };
 
 Vector.prototype.remove = function(index) {
@@ -45,10 +64,12 @@ Vector.prototype.remove = function(index) {
 };
 
 Vector.prototype.get = function(index) {
+    if (this.boundsCheck(index)) {return}
     return this.storage[index];
 };
 
 Vector.prototype.set = function(index, value) {
+    if (this.boundsCheck(index)) {return}
     this.storage[index] = value;
 };
 
@@ -61,6 +82,12 @@ Vector.prototype.contains = function (value) {
     return false
 };
 
+Vector.prototype.boundsCheck = function (index) {
+    if (index > this.length+1) {
+        console.error('you are out of bounds sir');
+        return true;
+    }
+};
 
 
 var v = new Vector();
@@ -78,5 +105,6 @@ console.log("v.set(2, 15): " + (v.get(2) === 15));
 v.remove(2);
 console.log("v.remove(2): " + (v.length === 5));
 console.log("contains(3): " + (v.contains(3)));
+console.log(v.insert(20, 2));
 
 
